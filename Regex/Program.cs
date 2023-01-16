026f63447a407b97ba89dd6854e863af73993bbe
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-01-16 08:13:31 +0530

    Validate first name

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace Regex
+﻿namespace Regexp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] str = {"Priyanshu",
+          "Naveen", "Rupali"};
+
+            foreach (string s in str)
+            {
+                Console.WriteLine("{0} {1} a valid Name.", s,
+                            RegularExpression.validation(s) ? "is" : "is not");
+            }
+
         }
     }
 }
---
+++ Regex/RegularExpression.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regexp
+{
+    internal class RegularExpression
+    {
+        public static bool validation(string validator)
+        {
+            string pattern = "^[A-Z]{1}[a-z]{2,10}$";
+
+            Regex reg = new Regex(pattern);
+
+            if (reg.IsMatch(validator))
+                return (true);
+            else
+                return (false);
+        }
+    }
+}
